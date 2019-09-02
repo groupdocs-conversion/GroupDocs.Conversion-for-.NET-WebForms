75f7609f69484f4b0389e070892b7c5f581f0876
Pavel Egorov <egorovpasha@gmail.com>
2019-09-02 14:04:10 +0400

    release

---
+++ src/Products/Conversion/Entity/Web/Request/ConversionPostedData.cs
@@ -0,0 +1,22 @@
+﻿using GroupDocs.Conversion.WebForms.Products.Common.Entity.Web;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace GroupDocs.Conversion.WebForms.Products.Conversion.Entity.Web.Request
+{
+    public class ConversionPostedData : PostedDataEntity
+    {
+        [JsonProperty]
+        private string destinationType { get; set; }     
+
+        public string GetDestinationType()
+        {
+            return this.destinationType;
+        }
+
+        public void SetDestinationType(string type)
+        {
+            this.destinationType = type;
+        }       
+    }
+}
