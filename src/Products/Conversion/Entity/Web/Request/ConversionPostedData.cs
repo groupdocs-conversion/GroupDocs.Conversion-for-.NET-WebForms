b59646eb7e5edded4fba5601dc2deed58f0bbe7e
Egor <egor.krushinov@aspose.com>
2020-04-09 20:29:24 +0300

    Initial changes to update API version.

--- src/Products/Conversion/Entity/Web/Request/ConversionPostedData.cs
+++ src/Products/Conversion/Entity/Web/Request/ConversionPostedData.cs
@@ -1,13 +1,15 @@
 ﻿using GroupDocs.Conversion.WebForms.Products.Common.Entity.Web;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 
 namespace GroupDocs.Conversion.WebForms.Products.Conversion.Entity.Web.Request
 {
     public class ConversionPostedData : PostedDataEntity
     {
         [JsonProperty]
-        private string destinationType { get; set; }     
+        private string destinationType { get; set; }
+
+        [JsonProperty]
+        private string destDocumentType { get; set; }
 
         public string GetDestinationType()
         {
@@ -17,6 +19,16 @@ namespace GroupDocs.Conversion.WebForms.Products.Conversion.Entity.Web.Request
         public void SetDestinationType(string type)
         {
             this.destinationType = type;
-        }       
+        }
+
+        public string GetDestDocumentType()
+        {
+            return this.destDocumentType;
+        }
+
+        public void SetDestDocumentType(string type)
+        {
+            this.destDocumentType = type;
+        }
     }
 }
